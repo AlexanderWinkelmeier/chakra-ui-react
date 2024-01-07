8fd45d2567153107b82bd1891a894765faba763e
Alexander Winkelmeier <R.A.Winkelmeier@hotmail.de>
2024-01-07 11:40:28 +0100

    Style Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
-import { Heading } from '@chakra-ui/react';
+import { Box, Text } from '@chakra-ui/react';
 
 function App() {
   return (
     <div className="App">
-      <Heading fontSize="56px">Create accessible React apps with speed</Heading>
+      <Box bg="green" w="200px" h="300px" margin="auto" mt="32px">
+        <Text fontSize="32px">This is a style props example</Text>
+      </Box>
     </div>
   );
 }
