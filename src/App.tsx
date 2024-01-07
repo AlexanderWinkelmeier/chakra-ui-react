54f183b5db8a0e58236fd0ad01fe1b7f0c3d59bf
Alexander Winkelmeier <R.A.Winkelmeier@hotmail.de>
2024-01-07 10:48:31 +0100

    Layout - flex

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import './App.css';
-import { Box } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 
 function App() {
   return (
     <div className="App">
-      <Box as="section" bg="green" w="120px" h="150px">
-        This is a box
-      </Box>
+      <Flex justify="space-between" align="center">
+        <Box bg="green" w="100px" h="200px">
+          item 1
+        </Box>
+        <Box bg="blue" w="100px" h="100px">
+          item 2
+        </Box>
+        <Box bg="red" w="200px" h="100px">
+          item 3
+        </Box>
+        <Box bg="tomato" w="100px" h="200px">
+          item 4
+        </Box>
+      </Flex>
     </div>
   );
 }
