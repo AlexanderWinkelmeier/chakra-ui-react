8290b01f9b5e506b403979e1577da0fc8e4bd1d8
Alexander Winkelmeier <R.A.Winkelmeier@hotmail.de>
2024-01-07 11:25:21 +0100

    Typography - Heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import './App.css';
-import { Text } from '@chakra-ui/react';
+import { Heading } from '@chakra-ui/react';
 
 function App() {
   return (
     <div className="App">
-      <Text as="i" noOfLines={1}>
-        "The quick brown fox jumps over the lazy dog" is an English-language
-        pangram—a sentence that contains all of the letters of the English
-        alphabet. Owing to its existence, Chakra was created.
-      </Text>
+      <Heading fontSize="56px">Create accessible React apps with speed</Heading>
     </div>
   );
 }
