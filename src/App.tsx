27e0041b77f93be0e13c43d072936b29fb320346
Alexander Winkelmeier <R.A.Winkelmeier@hotmail.de>
2024-01-07 11:50:32 +0100

    Style Props Pseudo - _hover and _focus

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import './App.css';
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Text, Button } from '@chakra-ui/react';
 
 function App() {
   return (
     <div className="App">
       <Box bg="green" w="200px" h="300px" margin="auto" mt="32px">
         <Text fontSize="32px">This is a style props example</Text>
+        <Button
+          mt="32px"
+          bgColor="red"
+          _hover={{ bgColor: 'blue' }}
+          _focus={{ bgColor: 'yellow', color: 'black' }}
+        >
+          Click me
+        </Button>
       </Box>
     </div>
   );
