84f91cec552093fbd763d4601059a73d7b0a8185
Alexander Winkelmeier <R.A.Winkelmeier@hotmail.de>
2024-01-07 12:30:25 +0100

    App - Header Section

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
-import './App.css';
-import { Box, Text, Button } from '@chakra-ui/react';
+import { Header } from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <Box bg="green" w="200px" h="300px" margin="auto" mt="32px">
-        <Text fontSize="32px">This is a style props example</Text>
-        <Button
-          mt="32px"
-          bgColor="red"
-          _hover={{ bgColor: 'blue' }}
-          _focus={{ bgColor: 'yellow', color: 'black' }}
-        >
-          Click me
-        </Button>
-      </Box>
+    <div>
+      <Header />
     </div>
   );
 }
